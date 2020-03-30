9370a61f968b31021231b73ab72959bc2c1ca6ed
unzoa <unzoa@icloud.com>
2020-03-30 18:11:26 +0800

    修改相同文件夹下子节点重复添加问题

--- filesPathTransformToTree.js
+++ filesPathTransformToTree.js
@@ -21,6 +21,7 @@
  *  }
  * ]
  */
+
 export function filesPathTransformToTree (files, message = '') {
   let treeData = []
   let id = 1
@@ -34,60 +35,73 @@ export function filesPathTransformToTree (files, message = '') {
     dirPathArr.forEach((dirPath, dirPathChildrenLevel) => {
       let isfirstLevelLabelExist = false // 第一层是否存在了
 
+      function addNode (children, isRoot) {
+        let hasNode = false
+        children.forEach(child => {
+          if (child.label === dirPath) {
+            hasNode = true
+          }
+        })
+        if (!hasNode) { // 判断节点不存在
+          id++
+          children.push({
+            id,
+            fullPath: isRoot ? dirPath : dirPathArr.slice(0, dirPathChildrenLevel).join('/') + '/' + dirPath,
+            label: dirPath,
+            children: [],
+            message,
+            ...externalData
+          })
+        }
+      }
+
       // 匹配超集
-      treeData.forEach(relativePathObj => { // {} || {label:'', children: []}
+      treeData.forEach(relativePathObj => { // {label:'', children: []}
         if (dirPathChildrenLevel === 0) { // 判断第一层是否存在这个label, 只判断一次
           if (dirPath === relativePathObj.label) {
             isfirstLevelLabelExist = true
             return false
           }
-        } else { // 深于第一层
-          let newChildren = relativePathObj.children // 超集的第一层的children: array
+        } else {
+          // 利用对象子集的拼接赋值
+          let newChildren = relativePathObj.children // 第一层children[array]
 
-          /**
-           * [level 查询在第几层children：利用对象子集的拼接赋值]
-           * @type {Number}
-           */
-          let level = 1
-          while (level < dirPathChildrenLevel) {
-            newChildren.forEach((child, childIndex) => {
-              newChildren = child.children || []
-            })
-            level++
-          }
-
-          if (newChildren.label === dirPathArr[dirPathChildrenLevel - 1] ||
-            !newChildren.label) { // 判断获取的children
-            // 判断子路径 label 是否存在
-            let flag = false
-            newChildren.forEach((child, childIndex) => {
-              flag = child.label === dirPath
-            })
-
-            if (!flag) {
-              id++
-              newChildren.push({
-                id,
-                fullPath: dirPathArr.slice(0, dirPathChildrenLevel).join('/') + '/' + dirPath,
-                label: dirPath,
-                children: [],
-                message,
-                ...externalData
+          switch (dirPathChildrenLevel) {
+            case 1:
+              let hasNode = false
+              newChildren.forEach(child => {
+                if (child.label === dirPath) {
+                  hasNode = true
+                  return false
+                }
               })
-            }
+              if (!hasNode) {
+                addNode(newChildren)
+              }
+              break
+
+            default:
+              let level = 1 // 每层只有一个children，查询在第几层children
+              while (level < dirPathChildrenLevel) { // dirPathChildrenLevel >= 2
+                newChildren.forEach((child, childIndex) => {
+                  if (child.label === dirPathArr[level]) { // 该层上包含这个path
+                    newChildren = child.children
+                    return false
+                  }
+                })
+                level++
+              }
+              // newChildren获取的正确，
+              // 但是需要判断label不存在才能执行addNode
+              addNode(newChildren)
+              break
           }
         }
       })
 
+      // 当tredata中空或者多个根部节点，必须要添加一条
       if (!isfirstLevelLabelExist && dirPathChildrenLevel === 0) {
-        treeData.push({
-          id,
-          fullPath: dirPath,
-          label: dirPath,
-          children: [],
-          message,
-          ...externalData
-        })
+        addNode(treeData, true)
       }
     })
   }
@@ -106,26 +120,53 @@ export function filesPathTransformToTree (files, message = '') {
     }
   }
 
-  return { treeData, treeNodelength: id }
+  return {
+    treeData: sortTreeData(treeData),
+    treeNodelength: id
+  }
+}
+
+function sortTreeData (treeData) {
+  // 排序第一层
+  for (let rootNode of treeData) {
+    let filesArr = []
+    let foldersArr = []
+
+    let children = rootNode.children
+    children.forEach(child => {
+      child.children.length
+        ? foldersArr.push(child)
+        : filesArr.push(child)
+    })
+    rootNode.children = [
+      ...foldersArr,
+      ...filesArr
+    ]
+  }
+  return treeData
 }
 
 /**
  * [getTreeNode 根据完整路径, 1:查找treeData的节点; 2:设置文件描述]
- * @param  {[type]} treeData [树]
- * @param  {[type]} filePath [文件的相对路径]
- * @param  {String} message  [设置节点在树上的描述]
- * @return {[type]}          [文件在树上的节点]
+ * @param  {[type]} treeData      [树]
+ * @param  {[type]} filePath      [文件的相对路径]
+ * @param  {String} message       [设置节点在树上的描述]
+ * @param  {Object} externalData  [节点上增加扩展数据]
+ * @return {[type]}               [文件在树上的节点]
  */
-export function getTreeNode (treeData, filePath, message = '') {
+export function getTreeNode (treeData, filePath, message = '', externalData = {}) {
   let nodeItem = {}
   let filePathArr = filePath.split('/')
 
-  // 匹配treeData
   // 查找第一层的children
   let newChildren = [] // 子集的继承
-  treeData.forEach(nodeItem => {
-    if (nodeItem.label === filePathArr[0]) { // 判断第一层存在在哪里
-      newChildren = nodeItem.children
+  treeData.forEach(treeNode => {
+    if (treeNode.label === filePathArr[0]) { // 判断第一层存在在哪里
+      if (filePathArr.length === 1) { // 避免的上传多文件的时间戳为同一个的错误
+        nodeItem = treeNode
+      } else {
+        newChildren = treeNode.children
+      }
       return false
     }
   })
@@ -138,11 +179,16 @@ export function getTreeNode (treeData, filePath, message = '') {
         // 判断子集的label在哪里
         if (child.label === path) {
           if (!child.children.length) {
-            nodeItem = child
-
             if (message) {
               child.message = message
             }
+
+            // 扩展数据
+            for (const i in externalData) {
+              child[i] = externalData[i]
+            }
+
+            nodeItem = child
           } else {
             newChildren = child.children
             searchNewChildren()
